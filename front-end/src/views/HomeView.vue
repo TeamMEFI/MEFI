f15baf6934a0cf79160c1b9acf4d0d9448e2b21f
jnsia <kia54682@gmail.com>
2024-01-15 16:18:22 +0900

    :art: : 기본 파일 삭제

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div>HOME</div>
   </main>
 </template>
+
+<script setup>
+</script>
