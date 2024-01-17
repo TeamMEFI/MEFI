33266d2c370d253561a119a0d1364ed0703e9573
최부광 <cbgbg0727@knu.ac.kr>
2024-01-17 23:09:19 +0900

    ✨: 추가 백엔드 빈 프로젝트 백엔드 개발을 위한 SpringBoot 프로젝트 생성

---
+++ back-end/src/test/java/com/mefi/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mefi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
