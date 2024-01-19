263d78bff1886748e50f9a091c39b3082f55d005
Lee <dqk8632@naver.com>
2024-01-19 21:56:33 +0900

    :construction: : setting
    alarm setting finish
    next input output setting
    Related to : #2

--- front-end/src/components/settings/setting/AlarmSetting.vue
+++ front-end/src/components/settings/setting/AlarmSetting.vue
@@ -1,11 +1,47 @@
 <template>
     <div>
-        <h1>alarm setting</h1>
+        <h1>알림 설정</h1>
+        <div>
+            <div id="item">
+                <div>알림 설정</div>
+                <div id="item-icon">
+                    <font-awesome-icon :icon="['fas', 'toggle-on']" v-if="alarm" @click="toggleAlarm"/>
+                    <font-awesome-icon :icon="['fas', 'toggle-off']" v-else @click="toggleAlarm"/>
+                </div>
+            </div>
+            <div id="item">
+                <div>알림 소리</div>
+                <div id="item-icon">
+                    <font-awesome-icon :icon="['fas', 'toggle-on']" v-if="sound" @click="toggleSound"/>
+                    <font-awesome-icon :icon="['fas', 'toggle-off']" v-else @click="toggleSound"/>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from "vue"
+
+const alarm = ref(true)
+const sound = ref(false)
+const toggleAlarm = () => {
+    alarm.value = !alarm.value
+}
+const toggleSound = () =>{
+    sound.value = !sound.value
+}
 </script>
 
 <style scoped>
+#item{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 10px;
+    border: 1px solid black;
+}
+#item-icon{
+    cursor: pointer;
+}
 </style>
