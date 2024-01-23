ac646c3560dd56c78b2e5a6d2d8d24cdca1b7700
Shin <syhan7516@naver.com>
2024-01-24 01:50:15 +0900

    ✨ : UserController 생성
    User에 관련된 컨트롤러 생성
    
    Related to: #S10P12D204-239

---
+++ backend/src/main/java/com/mefi/backend/api/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.mefi.backend.api.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@Tag(name="1.USER", description="USER API")
+public class UserController {
+
+}
