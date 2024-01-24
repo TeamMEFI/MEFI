aac522da75dc834dfb1711b468de9d468c73d19a
Shin <syhan7516@naver.com>
2024-01-25 01:14:16 +0900

    ✨ : LoginFilter 생성
    아이디, 비밀번호 검증을 위해 기존 필터를 상속받아 필터 작성
    
    Related to: #S10P12D204-309

---
+++ backend/src/main/java/com/mefi/backend/common/auth/LoginFilter.java
@@ -0,0 +1,45 @@
+package com.mefi.backend.common.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response) throws AuthenticationException {
+
+        // 요청을 가로채 클라이언트 요청에서 username (userEmail), password (userPassword) 추출
+        String userEmail = obtainUsername(request);
+        String userPassword = obtainPassword(request);
+
+        // username (userEmail), password (userPassword) 검증하기 위해 authToken 객체에 담기
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userEmail, userPassword, null);
+
+        // 검증을 위해 authToken을 AuthenticationManager로 전달
+        return authenticationManager.authenticate(authToken);
+    }
+
+    // 로그인 성공시 실행하는 메소드
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+    }
+
+    // 로그인 실패시 실행하는 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+    }
+}
