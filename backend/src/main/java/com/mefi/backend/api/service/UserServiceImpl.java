1a59b21943c81ecac4823e8df1d8bee17a436047
Shin <syhan7516@naver.com>
2024-01-24 16:45:05 +0900

    ✨ : 회원가입 기능 구현
    Related to: #S10P12D204-241

--- backend/src/main/java/com/mefi/backend/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/mefi/backend/api/service/UserServiceImpl.java
@@ -1,10 +1,33 @@
 package com.mefi.backend.api.service;
 
+import com.mefi.backend.api.request.JoinReqDto;
+import com.mefi.backend.db.entity.User;
+import com.mefi.backend.db.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
+    // 회원가입
+    @Override
+    public void join(JoinReqDto joinReqDto) {
+        
+        // 가입 정보 입력
+        User user = User.builder()
+                .email(joinReqDto.getEmail())
+                .password(joinReqDto.getPassword())
+                .name(joinReqDto.getName())
+                .position(joinReqDto.getPosition())
+                .dept(joinReqDto.getDept())
+                .build();
+
+        // DB 저장
+        userRepository.save(user);
+    }
 }
