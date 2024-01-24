25e5ede6e7c36990babe1588ff935a5a66d24c93
Shin <syhan7516@naver.com>
2024-01-25 02:54:00 +0900

    ✨ : userDetailsService 생성
    사용자 정보를 데이터베이스에서 조회하여 전달하기 위해 생성
    이메일 기반으로 사용자를 조회 가능하도록 메서드 작성
    
    Related to: #S10P12D204-307

--- backend/src/main/java/com/mefi/backend/db/repository/UserRepository.java
+++ backend/src/main/java/com/mefi/backend/db/repository/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    // 이메일로 회원 조회
+    User findByEmail(String email);
 }
