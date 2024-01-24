1190ce6f271168c20a98a0d58b41232056f41290
Shin <syhan7516@naver.com>
2024-01-25 01:28:17 +0900

    ✨ : SecurityConfig LoginFilter, AuthenticationManager 등록
    LoginFilter 인자로 전달하기 위해 AuthenticationManager 등록
    아이디, 비밀번호 검증을 위해 기존의 필터 자리에 LoginFilter 대체 등록
    
    Related to: #S10P12D204-309

--- backend/src/main/java/com/mefi/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/mefi/backend/config/SecurityConfig.java
@@ -1,16 +1,31 @@
 package com.mefi.backend.config;
 
+import com.mefi.backend.common.auth.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    // AuthenticationManager 등록
+    // 데이터베이스에서 회원 정보를 가져와 검증 수행하는 클래스
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     // BCryptPaasswordEncoder 등록
     // 비밀번호 인코딩(암호화) 기능을 구현한 클래스
@@ -45,6 +60,11 @@ public class SecurityConfig {
 
                         // 이외 인증된 사용자만 접근 허용
                         .anyRequest().authenticated());
+
+        // 필터 등록
+        http
+                // 해당 필터 자리에서 수행 (수행 할 필터, 대체 필터 자리)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         // 세션 설정 (JWT를 통한 인증/인가를 위해서 세션을 STATELESS 상태로 설정)
         http
