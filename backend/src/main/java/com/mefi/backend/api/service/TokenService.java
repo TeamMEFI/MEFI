c7a0cddd615989046b79dd6a60efbd8e486104d0
신영한 <syhan7516@naver.com>
2024-01-29 21:30:13 +0900

    ✨: TokenService, TokenServiceImpl 엑세스 토큰 재발급 메서드 구현 엑세스 토큰 재발급 메서드 구현  : 전달 인자 - 유저, 리프레시 토큰  : 반환 값 - CreateAccessTokenResDto (엑세스 토큰을 가진 Dto)
    Related to: #S10P12D204-373

--- backend/src/main/java/com/mefi/backend/api/service/TokenService.java
+++ backend/src/main/java/com/mefi/backend/api/service/TokenService.java
@@ -1,4 +1,10 @@
 package com.mefi.backend.api.service;
 
+import com.mefi.backend.api.response.CreateAccessTokenResDto;
+import com.mefi.backend.db.entity.User;
+
 public interface TokenService {
+
+    // 엑세스 토큰 재발급
+    CreateAccessTokenResDto createAccessToken(User user, String refreshToken);
 }
--- backend/src/main/java/com/mefi/backend/api/service/TokenServiceImpl.java
+++ backend/src/main/java/com/mefi/backend/api/service/TokenServiceImpl.java
@@ -1,7 +1,27 @@
 package com.mefi.backend.api.service;
 
+import com.mefi.backend.api.response.CreateAccessTokenResDto;
+import com.mefi.backend.common.util.JWTUtil;
+import com.mefi.backend.db.entity.User;
+import com.mefi.backend.db.repository.TokenRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TokenServiceImpl implements TokenService {
+
+    private final JWTUtil jwtUtil;
+    private final TokenRepository tokenRepository;
+
+    // 엑세스 토큰 재발급
+    @Override
+    @Transactional
+    public CreateAccessTokenResDto createAccessToken(User user, String refreshToken) {
+
+        // 엑세스 토큰 재발급 및 저장
+        String accessToken = jwtUtil.createJwt(user.getEmail(), user.getPosition(), 60*5*1000L);
+        return new CreateAccessTokenResDto(accessToken);
+    }
 }
