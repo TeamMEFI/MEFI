b1720267c88551b7ce3551a8e0d38526bc685593
신영한 <syhan7516@naver.com>
2024-01-30 02:47:23 +0900

    ✨: UserController 회원탈퇴 API 구현
    회원탈퇴 API 구현
     : 로그인된 유저 식별 ID로 유저 조회 후 탈퇴 수행
     : 상태 200 OK, Success 반환
    
    Related to: #S10P12D204-342

--- backend/src/main/java/com/mefi/backend/api/controller/UserController.java
+++ backend/src/main/java/com/mefi/backend/api/controller/UserController.java
@@ -6,11 +6,11 @@ import com.mefi.backend.api.service.UserService;
 import com.mefi.backend.common.auth.CustomUserDetails;
 import com.mefi.backend.common.model.BaseResponseBody;
 import com.mefi.backend.db.entity.Token;
+import com.mefi.backend.db.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -33,6 +33,23 @@ public class UserController {
         // 회원가입
         userService.join(joinReqDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponseBody.of(0,"Success"));
+    }
+
+    @Operation(summary = "회원탈퇴", description = "/users\n\n 사용자는 회원탈퇴를 한다.")
+    @DeleteMapping("")
+    @ApiResponse(responseCode = "200", description = "성공 \n\n Success 반환")
+    public ResponseEntity<? extends BaseResponseBody> withdraw(Authentication authentication) {
+
+        // 로그인된 유저 정보 조회
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        // 유저 식별 ID로 유저 정보 조회
+        User user = userService.findById(userDetails.getUserId());
+
+        // 유저 제거
+        userService.withdraw(user);
+
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0,"Success"));
     }
 
     @Operation(summary = "로그아웃", description = "/users/logout\n\n 사용자는 로그아웃을 한다.")
