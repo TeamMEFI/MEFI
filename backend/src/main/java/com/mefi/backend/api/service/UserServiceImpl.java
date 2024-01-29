9fa523639767c8d86e1b35f9c89a373c6644fa36
신영한 <syhan7516@naver.com>
2024-01-29 21:17:42 +0900

    ✨: UserService, CustomUserDetails 유저관련 조회 기능 구현
    UserService, UserServiceImpl
     : 유저 식별 ID로 유저 조회 메서드 구현
     : 전달 인자 - 유저 식별 ID
     : 반환 값 - Optional<User>
    CustomUserDetails
     : 로그인된 유저의 식별 ID 조회 메서드 구현
     : 전달 인자 - X
     : 반환 값 - Long userId
    
    Related to: #S10P12D204-373

--- backend/src/main/java/com/mefi/backend/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/mefi/backend/api/service/UserServiceImpl.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -31,5 +33,11 @@ public class UserServiceImpl implements UserService {
 
         // DB 저장
         userRepository.save(user);
+    }
+
+    // 식별 ID로 유저 조회
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
     }
 }
