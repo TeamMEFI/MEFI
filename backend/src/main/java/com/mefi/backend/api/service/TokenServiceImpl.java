00feb2c04f492b49f64165b2e6d02f756bd3a3cb
신영한 <syhan7516@naver.com>
2024-01-29 23:15:37 +0900

    ✨: TokenService, TokenSeriveImpl 토큰 조회, 토큰 엔티티 제거 메서드 구현
    유저 식별 ID로 토큰 조회 메서드 구현
     : 전달 인자 - 유저 식별 ID
     : 반환 값 - Token
    
    토큰 엔티티 제거 메서드 구현
     : 전달 인자 - Token
     : 반환 값 - X
    
    Related to: #S10P12D204-341

--- backend/src/main/java/com/mefi/backend/api/service/TokenServiceImpl.java
+++ backend/src/main/java/com/mefi/backend/api/service/TokenServiceImpl.java
@@ -2,6 +2,7 @@ package com.mefi.backend.api.service;
 
 import com.mefi.backend.api.response.CreateAccessTokenResDto;
 import com.mefi.backend.common.util.JWTUtil;
+import com.mefi.backend.db.entity.Token;
 import com.mefi.backend.db.entity.User;
 import com.mefi.backend.db.repository.TokenRepository;
 import jakarta.transaction.Transactional;
@@ -23,5 +24,18 @@ public class TokenServiceImpl implements TokenService {
         // 엑세스 토큰 재발급 및 저장
         String accessToken = jwtUtil.createJwt(user.getEmail(), user.getPosition(), 60*5*1000L);
         return new CreateAccessTokenResDto(accessToken);
+    }
+
+    // 유저 식별 ID로 토큰 조회
+    @Override
+    public Token findByUserId(Long id) {
+        return tokenRepository.findByUserId(id)
+                .orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    // 리프레시 토큰 제거
+    @Override
+    public void deleteRefreshToken(Token token) {
+        tokenRepository.delete(token);
     }
 }
