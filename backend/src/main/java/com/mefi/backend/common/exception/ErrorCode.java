3a982b16400aa5a94f9785abba811bf9a47135b3
신영한 <syhan7516@naver.com>
2024-01-31 16:01:38 +0900

    ✨: 에러 코드 열거 타입 추가
    CODE_NOT_MATCH
     : 인증 코드가 일치하지 않았을 경우 처리
    
    UNEXPECTED_TOKEN
     : 토큰이 만료되었거나, 존재하지 않았을 경우 처리
    
    Related to: #S10P12D204-383

--- backend/src/main/java/com/mefi/backend/common/exception/ErrorCode.java
+++ backend/src/main/java/com/mefi/backend/common/exception/ErrorCode.java
@@ -10,7 +10,11 @@ public enum ErrorCode {
     // 에러 열거
 
     // 회원
-    USER_NOT_EXIST(HttpStatus.BAD_REQUEST, "U-001", "존재하지 않는 회원입니다.");
+    USER_NOT_EXIST(HttpStatus.BAD_REQUEST, "U-001", "존재하지 않는 회원입니다."),
+    CODE_NOT_MATCH(HttpStatus.BAD_REQUEST, "U-002", "인증 코드가 일치하지 않습니다."),
+
+    // 토큰
+    UNEXPECTED_TOKEN(HttpStatus.BAD_REQUEST, "T-001", "만료되었거나 존재하지 않는 토큰입니다.");
 
     // 상태, 에러 코드, 메시지
     private HttpStatus httpStatus;
