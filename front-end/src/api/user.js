f8844e2abae234bedce26d8eae7a19b80bb2188d
이지연 <dqk8632@naver.com>
2024-01-31 14:34:51 +0900

    ✨ : 로그인 회원가입 api 연결 완료
    회원가입시, 회원 생성 및 로그인 함수 호출
    로그인시, 토큰 및 회원 정보 저장
    토큰은 로컬세션에 저장
    회원 정보는 store에 저장
    메인페이지로 이동
    S10P12D204-382 #done
    S10P12D204-323 #done
    S10P12D204-327 #done

--- front-end/src/api/user.js
+++ front-end/src/api/user.js
@@ -2,9 +2,7 @@ import { localAxios } from "@/util/http-commons";
 const local = localAxios();
 
 async function userLogin(param, success, fail){
-    console.log('api userLogin',param)
     await local.post(`/users/login`, param).then(success).catch(fail);
-    console.log('api userLogin success')
 }
 async function userSignup(param, success, fail){
     await local.post(`/users`, param).then(success).catch(fail);
--- front-end/src/stores/user.js
+++ front-end/src/stores/user.js
@@ -4,43 +4,41 @@ import { useRouter } from 'vue-router'
 import { userLogin, userSignup } from '@/api/user.js'
 
 // user store
-// login, signup, user info, token regenerate
+// login, signup, user info, 토큰 관리
 export const useUserStore = defineStore('user', () => {
-
   const isLogin = ref(false)
   const router = useRouter()
-  const userInfo = ref({
-    email:'ooo@gmail.com',
-    name:'ooo',
-    department:'department',
-    position:'position',
-    status:'green',
-  })
+  const userInfo = ref(null)
 
   // 회원 가입 함수
   // user 정보 : email, password, name, position, department
   const signup = async (user) => {
     await userSignup(
       user,(response)=>{
-        console.log(response)
-        
-        isLogin.value = true
-        router.push({name:'home'})
       },
       (error)=>{
         console.log(error)
       }
     )
+     .then(()=>{
+      const loginUser = {
+        email:user.email,
+        password:user.password,
+      }
+      login(loginUser)
+     })
   }
 
   // 로그인 함수
   // user 정보 : email, password
   const login = async (user) => {
-    console.log('store', user)
-    await userLogin(user)(
+    console.log(user)
+    await userLogin(
       user,(response) => {
-        console.log(response)
-        isLogin.value = true
+        userInfo.value = response.data.dataBody
+        localStorage.setItem("accessToken", response.headers.accesstoken)
+        localStorage.setItem("refreshToken", response.headers.refreshtoken)
+        isLogin.value = true;
         router.push({name:'home'})
       },
       (error)=>{
@@ -52,7 +50,6 @@ export const useUserStore = defineStore('user', () => {
   // 로그아웃 함수
   // isLogin, token delete
   const logout = () => {
-    console.log('log out')
     isLogin.value = false
     localStorage.clear()
     router.push({name:'home'})
