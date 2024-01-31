871b6bae7ded95ae0ef67b294db16625083de425
신영한 <syhan7516@naver.com>
2024-01-31 13:21:52 +0900

    ✨: 이메일 인증 메일 전송 처리
    EmailAuth 변경
     : email 필드 추가
     : 빌드 패턴 추가
     : 인증 코드 업데이트 메서드 추가
    
    EmailConfig
     : yml 데이터 가져오기
     : 메일 정보 설정
     : 메일 정보 부가 설정
    
    MailService, MailServiceImpl
     : 인증 코드 생성 메서드 구현
     : 메일 내용 생성 메서드 구현
     : 메일 전송 메서드 구현
    
    MailRepository
     : 이메일로 인증 엔티티 조회 메서드 추가
    
    UserController 이메일 인증 API 구현
     : 이메일 인증을 위한 메일 전송
     : 상태 201 CREATED, Success 반환
    
    Related to: #S10P12D204-379

--- backend/src/main/java/com/mefi/backend/api/service/MailService.java
+++ backend/src/main/java/com/mefi/backend/api/service/MailService.java
@@ -1,5 +1,18 @@
 package com.mefi.backend.api.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+import java.io.UnsupportedEncodingException;
+
 public interface MailService {
 
+    // 인증 코드 생성
+    public String createAuthCode();
+
+    // 메일 내용 생성
+    public MimeMessage createMessage(String receiver, int authCode) throws MessagingException, UnsupportedEncodingException;
+
+    // 메일 전송
+    public int sendMessage(String email) throws MessagingException, UnsupportedEncodingException;
 }
