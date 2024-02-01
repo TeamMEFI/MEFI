50012121c45bf0e975741bacefdb1c765f99d70d
신영한 <syhan7516@naver.com>
2024-02-01 16:51:36 +0900

    ✨: 검색어로 유저 조회 Service 메서드 구현
    검색어를 입력 받아 이름 또는 이메일 중 해당 검색어를 포함하는 유저들을 반환
     : 전달 인자 - 검색 키워드
     : 반환 값 - 키워드에 적절한 유저들을 반환
    
    Related to: #S10P12D204-387

--- backend/src/main/java/com/mefi/backend/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/mefi/backend/api/service/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.mefi.backend.api.service;
 
 import com.mefi.backend.api.request.JoinReqDto;
+import com.mefi.backend.api.response.MemberResDto;
 import com.mefi.backend.db.entity.User;
 import com.mefi.backend.db.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -45,5 +48,11 @@ public class UserServiceImpl implements UserService {
     public User findById(Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    // 검색어로 유저 조회
+    @Override
+    public List<MemberResDto> getSearchUsers(String keyword) {
+        return userRepository.findByKeyWord(keyword);
     }
 }
