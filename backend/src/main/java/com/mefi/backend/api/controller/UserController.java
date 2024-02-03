4ab7dbe1ccbb3bb727752593ec9886d00fdaf36f
신영한 <syhan7516@naver.com>
2024-02-03 23:29:26 +0900

    🚑️: 회원 조회 API 에러 발생
    문제 : 회원 조회 API 요청시 요청 불가
    원인 : @PathVariable 값 미지정으로 에러 발생
    해결 : @PathVariable 값을 keyword로 지정
    
    Related to: #S10P12D204-387

--- backend/src/main/java/com/mefi/backend/api/controller/UserController.java
+++ backend/src/main/java/com/mefi/backend/api/controller/UserController.java
@@ -12,9 +12,11 @@ import com.mefi.backend.common.model.BaseResponseBody;
 import com.mefi.backend.db.entity.Token;
 import com.mefi.backend.db.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -39,7 +41,7 @@ public class UserController {
 
         // 회원가입
         userService.join(joinReqDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponseBody.of(0,"Success"));
+        return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponseBody.of(0, "Success"));
     }
 
     @Operation(summary = "회원탈퇴", description = "/users\n\n 사용자는 회원탈퇴를 한다.")
@@ -56,7 +58,7 @@ public class UserController {
         // 유저 제거
         userService.withdraw(user);
 
-        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0,"Success"));
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, "Success"));
     }
 
     @Operation(summary = "로그아웃", description = "/users/logout\n\n 사용자는 로그아웃을 한다.")
@@ -70,7 +72,7 @@ public class UserController {
         // 식별 ID로 리프레시 토큰 제거
         Token token = tokenService.findByUserId(userDetails.getUserId());
         tokenService.deleteRefreshToken(token);
-        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0,"Success"));
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, "Success"));
     }
 
     @Operation(summary = "이메일 인증", description = "/users/join/auth\n\n 사용자는 이메일 인증을 한다.")
@@ -80,7 +82,7 @@ public class UserController {
 
         // 메일 전송 후 코드 받기
         mailService.sendMessage(verifyEmailReqDto.getEmail());
-        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0,"Success"));
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, "Success"));
     }
 
     @Operation(summary = "이메일 인증 확인", description = "/users/join/auth/check\n\n 사용자는 이메일 인증 확인을 한다.")
@@ -90,16 +92,18 @@ public class UserController {
 
         // 인증 코드 확인 후 토큰 반환
         String token = mailService.validateAuthCode(verifyCodeReqDto);
-        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0,token));
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, token));
     }
 
     @Operation(summary = "회원 검색", description = "/users/search/{keyword} \n\n 사용자는 회원을 검색한다.")
     @GetMapping("/search/{keyword}")
-    @ApiResponse(responseCode = "200", description = "성공 \n\n 검색 결과 리스트 반환")
-    public ResponseEntity<? extends BaseResponseBody> searchUsers(@PathVariable String keyword) {
+    @ApiResponse(responseCode = "200", description = "성공 \n\n 검색 결과 유저 리스트 반환")
+    public ResponseEntity<? extends BaseResponseBody> searchUsers(
+            @Parameter(name = "keyword", description = "검색 키워드")
+            @PathVariable("keyword") String keyword) {
 
         // 검색어로 회원 조회
         List<MemberResDto> searchResultList = userService.getSearchUsers(keyword);
-        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0,searchResultList));
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, searchResultList));
     }
 }
