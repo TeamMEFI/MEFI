f9c802a6c260a8ded20bb55d6ac7c29bdc378f99
신영한 <syhan7516@naver.com>
2024-02-04 16:08:11 +0900

    ✨: 에러 코드 열거 타입 추가
    CATEGORY_NOT_EXIST
     : 회원 정보 수정시 존재하지 않는 항목의 수정을 요청한 경우 처리
    
    Related to: #S10P12D204-383

--- backend/src/main/java/com/mefi/backend/common/exception/ErrorCode.java
+++ backend/src/main/java/com/mefi/backend/common/exception/ErrorCode.java
@@ -17,6 +17,7 @@ public enum ErrorCode {
     CODE_NOT_MATCH(HttpStatus.BAD_REQUEST, "U-005", "인증 코드가 일치하지 않습니다."),
     CODE_NOT_EXIST(HttpStatus.BAD_REQUEST, "U-006", "인증 코드가 유효하지 않습니다."),
     SAME_AS_BEFORE(HttpStatus.BAD_REQUEST, "U-007", "이전 정보와 동일합니다."),
+    CATEGORY_NOT_EXIST(HttpStatus.BAD_REQUEST, "U-008", "존재하지 않는 항목입니다."),
 
     // 토큰
     UNEXPECTED_TOKEN(HttpStatus.BAD_REQUEST, "T-001", "만료되었거나 존재하지 않는 토큰입니다."),
