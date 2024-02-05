b3bcbdb25ba0a33f3f7acb60b411472bd2e57600
신영한 <syhan7516@naver.com>
2024-02-05 19:26:48 +0900

    🔨: 회원탈퇴시 입력된 비밀번호와 현재 비밀번호를 비교하는 코드 추가
    입력된 비밀번호와 현재 비밀번호 비교하는 코드 추가
     : 회원탈퇴 전 입력받은 비밀번호 확인
     : 입력 값이 현재 비밀번호와 불일치할 경우 예외 발생 (상태 400 Bad Request, error code & message 반환)
    
    Related to: #S10P12D204-343

--- backend/src/main/java/com/mefi/backend/api/service/UserService.java
+++ backend/src/main/java/com/mefi/backend/api/service/UserService.java
@@ -3,6 +3,7 @@ package com.mefi.backend.api.service;
 import com.mefi.backend.api.request.JoinReqDto;
 import com.mefi.backend.api.request.UserModifyPasswordReqDto;
 import com.mefi.backend.api.request.UserModifyReqDto;
+import com.mefi.backend.api.request.UserWithdrawReqDto;
 import com.mefi.backend.api.response.MemberResDto;
 import com.mefi.backend.db.entity.User;
 
@@ -14,7 +15,7 @@ public interface UserService {
     void join(JoinReqDto joinReqDto);
 
     // 회원탈퇴
-    void withdraw(User user);
+    void withdraw(User user, UserWithdrawReqDto userWithdrawReqDto);
 
     // 식별 ID로 유저 조회
     User findById(Long id);
