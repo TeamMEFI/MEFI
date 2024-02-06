ca3225e2c1beecd64ad33c4d9636d8beba92816b
이지연 <dqk8632@naver.com>
2024-02-06 10:11:23 +0900

    ✨ : api 붙이기 끝

--- front-end/src/api/file.js
+++ front-end/src/api/file.js
@@ -4,25 +4,25 @@ const local = localAxios()
 
 const getFiles = async (param, conferenceId, success, fail) => {
   local.defaults.headers['Authorization'] = 'Bearer ' + localStorage.getItem('accessToken')
-  await local.get(`/file/all/${conferenceId}`).then(success).catch(fail)
+  await local.get(`/api/file/all/${conferenceId}`).then(success).catch(fail)
 }
 
 const createFile = async (param, success, fail) => {
   local.defaults.headers['Authorization'] = 'Bearer ' + localStorage.getItem('accessToken')
   local.defaults.headers['Content-Type'] = 'multipart/form-data'
-  await local.post(`/file`, param).then(success).catch(fail)
+  await local.post(`/api/file`, param).then(success).catch(fail)
 }
 
 const downloadFile = async (param, conferenceId, success, fail) => {
   local.defaults.headers['Authorization'] = 'Bearer ' + localStorage.getItem('accessToken')
   // 바이너리 데이터를 response로 받기 위한 설정
   local.defaults['responseType'] = 'blob'
-  await local.get(`/file/download/${conferenceId}`, { params: param }).then(success).catch(fail)
+  await local.get(`/api/file/download/${conferenceId}`, { params: param }).then(success).catch(fail)
 }
 
 const deleteFile = async (param, conferenceId, success, fail) => {
   local.defaults.headers['Authorization'] = 'Bearer ' + localStorage.getItem('accessToken')
-  await local.delete(`/file/${conferenceId}`, { params: param }).then(success).catch(fail)
+  await local.delete(`/api/file/${conferenceId}`, { params: param }).then(success).catch(fail)
 }
 
 export { getFiles, createFile, downloadFile, deleteFile }
