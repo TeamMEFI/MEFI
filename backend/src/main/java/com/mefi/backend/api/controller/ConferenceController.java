7216194f86edf0c5f687643e37969afc231161a1
신영한 <syhan7516@naver.com>
2024-02-07 20:01:32 +0900

    ✨: 회의 생성, 예약 API & 메서드 구현
    ConferenceController
     : 리더에게 회의 정보를 받아 회의를 생성하는 API
     : 상태 201 CREATED, Success 반환
    
     ConferenceService, ConferenceServiceImple
     : 사용자 식별 ID와 회의 정보를 전달 받아 회의를 생성
     : 전달 인자 - 사용자 식별 ID, 회의 정보
     : 반환 값 - X
    
    Related to: #S10P12D204-423

--- backend/src/main/java/com/mefi/backend/api/controller/ConferenceController.java
+++ backend/src/main/java/com/mefi/backend/api/controller/ConferenceController.java
@@ -8,29 +8,31 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/meeting")
+@RequestMapping("/api/meeting")
 @Tag(name="3.CONFERENCE", description="CONFERENCE API")
 public class ConferenceController {
 
     private final ConferenceService conferenceService;
 
-    @Operation(summary = "회의 생성", description = "/meeting/{teamId}\n\n 리더는 회의를 생성 가능하다.")
-    @PostMapping("/{teamId}")
+    @Operation(summary = "회의 생성", description = "api/meeting/\n\n 리더는 회의를 생성 가능하다.")
+    @PostMapping("")
     @ApiResponse(responseCode = "201", description = "성공 \n\n Success 반환")
     public ResponseEntity<? extends BaseResponseBody> createMeeting(Authentication authentication,
                                                                     @RequestBody ConferenceCreateReqDto conferenceCreateReqDto) {
         // 로그인된 유저 정보 조회
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        log.info("\n회의 생성 URL 맵핑: {}", conferenceCreateReqDto.getTitle());
+        log.info("\n회의 생성자 : {}", userDetails.getUserId());
 
         // 회의 생성
         conferenceService.createMeeting(userDetails.getUserId(), conferenceCreateReqDto);
