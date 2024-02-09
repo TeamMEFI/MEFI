6f3de044c38975a6c47b8f7ed33c364dac4f78a1
신영한 <syhan7516@naver.com>
2024-02-09 20:44:30 +0900

    ✨: 회의 식별 ID로 회의 조회하는 Repository 메서드 구현
    Repository에서 회의 식별 ID로 회의를 가져오는 메서드 구현
     : 전달 인자 - 회의 식별 ID
     : 반환 값 - 회의
    
    Related to: #S10P12D204-350

--- backend/src/main/java/com/mefi/backend/db/repository/ConferenceRepository.java
+++ backend/src/main/java/com/mefi/backend/db/repository/ConferenceRepository.java
@@ -3,6 +3,10 @@ package com.mefi.backend.db.repository;
 import com.mefi.backend.db.entity.Conference;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ConferenceRepository extends JpaRepository<Conference, Long> {
 
+    // 회의 식별 ID로 회의 정보 조회
+    Optional<Conference> findById(Long conferencId);
 }
